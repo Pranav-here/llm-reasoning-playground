38ee924f372d38876b253011a4c078b49c2ceeb0
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-07-14 15:26:03 +0530

    Setup question box and options

--- app.py
+++ app.py
@@ -4,10 +4,19 @@ import openai
 import groq
 import dotenv
 
-dotenv.loadenv()
+dotenv.load_dotenv()
 
 
 # Set up the env variables
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+
+st.set_page_config(page_title="Chain of Thought Demo", layout='centered')
+st.title('Chain of Thought vs Direct Prompting')
+
+question=st.text_area("Enter a question:", placeholder="Whats 12 times 17?")
+
+model_choice=st.selectbox("Choose model:", ["OpenAI (GPT 3.5)", "Groq (Mistral)"])
+mode=st.radio("Choose prompting style:", ["Direct", "Chain of Thought"])
+
 
